f50d57bb1da178c43ebfaa6ea5c076ddbd63979a
rainbow <1351652209@qq.com>
2019-06-17 16:09:40 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+/**
+ * @Author GuoHui
+ * @Date 2019/6/17
+ */
+public class DemoController {
+
+    public static void main(String[] args) {
+
+        System.out.println("This a demo!!!");
+
+    }
+
+}
